113f1ec6c3beb5bf18110459a7a52a1b9ba79585
Gianluca Cioni <gianlucacioni93@gmail.com>
2021-07-18 02:13:48 +0200

    made operations object within calculator

--- index.js
+++ index.js
@@ -1,31 +1,38 @@
 calculator = {
-    addition: function(a,b){
-        return a + b;
-    },
-    
-    subtraction: function(a,b) {
-        return a - b;
-    },
-    
-    multiplication: function(a,b) {
-        return a * b;
-    },
-    
-    division: function(a,b) {
-        return a / b;
-    },
-    
-    operate: function(operator, a, b) {
-        if (operator == "+") {
-            return this.addition(a,b);
-        } else if (operator == "-") {
-            return this.subtraction(a,b);
-        } else if (operator == "*") {
-            return this.multiplication(a,b);
-        } else if (operator == "/") {
-            return this.division(a,b);
-        } else {
-            return "ERROR";
-        }
-    },
-}
+    operations: {
+        addition: function(a,b){
+            return a + b;
+        },
+        
+        subtraction: function(a,b) {
+            return a - b;
+        },
+        
+        multiplication: function(a,b) {
+            return a * b;
+        },
+        
+        division: function(a,b) {
+            return a / b;
+        },
+        
+        operate: function(operator, a, b) {
+            if (operator == "+") {
+                return this.addition(a,b);
+            } else if (operator == "-") {
+                return this.subtraction(a,b);
+            } else if (operator == "*") {
+                return this.multiplication(a,b);
+            } else if (operator == "/") {
+                return this.division(a,b);
+            } else {
+                return "ERROR";
+            }
+        },
+    }
+
+};
+
+
+
+
