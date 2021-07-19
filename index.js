170719db0863c20819db08358e81eb6200ceed0e
Gianluca Cioni <gianlucacioni93@gmail.com>
2021-07-19 03:50:03 +0200

    added comments and improved some variable names

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+// Define global variables
+
 let firstOperand = 0
 
 let secondOperand = 0
@@ -6,7 +8,9 @@ let tempOperator = 0
 
 let display = document.querySelector("#display")
 
-allClearBtn.addEventListener("click", () => calculator.functions.clear())
+// Give each button a function
+
+allClearBtn.addEventListener("click", () => calculator.functions.allClear())
 
 equalsBtn.addEventListener("click", () => calculator.operations.equals(tempOperator, firstOperand))
 
@@ -20,78 +24,77 @@ subtractionBtn.addEventListener("click", () => calculator.functions.operation("-
 
 plusMinusBtn.addEventListener("click", () => calculator.functions.plusMinus())
 
-zero.addEventListener("click", () => calculator.functions.btnInput(0))
+zero.addEventListener("click", () => calculator.functions.displayInput(0))
 
-one.addEventListener("click", () => calculator.functions.btnInput(1))
+one.addEventListener("click", () => calculator.functions.displayInput(1))
 
-two.addEventListener("click", () => calculator.functions.btnInput(2))
+two.addEventListener("click", () => calculator.functions.displayInput(2))
 
-three.addEventListener("click", () => calculator.functions.btnInput(3))
+three.addEventListener("click", () => calculator.functions.displayInput(3))
 
-four.addEventListener("click", () => calculator.functions.btnInput(4))
+four.addEventListener("click", () => calculator.functions.displayInput(4))
 
-five.addEventListener("click", () => calculator.functions.btnInput(5))
+five.addEventListener("click", () => calculator.functions.displayInput(5))
 
-six.addEventListener("click", () => calculator.functions.btnInput(6))
+six.addEventListener("click", () => calculator.functions.displayInput(6))
 
-seven.addEventListener("click", () => calculator.functions.btnInput(7))
+seven.addEventListener("click", () => calculator.functions.displayInput(7))
 
-eight.addEventListener("click", () => calculator.functions.btnInput(8))
+eight.addEventListener("click", () => calculator.functions.displayInput(8))
 
-nine.addEventListener("click", () => calculator.functions.btnInput(9))
+nine.addEventListener("click", () => calculator.functions.displayInput(9))
 
-point.addEventListener("click", () => calculator.functions.btnInput("."))
+point.addEventListener("click", () => calculator.functions.displayInput("."))
 
-
-
+// Create calculator object
 
 calculator = {
+
+    // Contains all operation related functions
+
     operations: {
         addition: function(a,b){
             return a + b;
-        },
-        
+        },        
         subtraction: function(a,b) {
             return a - b;
         },
-        
         multiplication: function(a,b) {
             return a * b;
-        },
-        
+        },     
         division: function(a,b) {
             return a / b;
         },
-
         equals: function(tempOperator,firstOperand) {
             secondOperand = parseFloat(display.textContent);
             return calculator.operations.operate(tempOperator,firstOperand,secondOperand)
-        },
-        
+        },      
         operate: function(tempOperator, a, b) {
             if (tempOperator == "+") {
                 display.textContent = this.addition(a,b);
-                calculator.functions.displayLength();
+                calculator.functions.displayMaxLength();
             } else if (tempOperator == "-") {
                 display.textContent = this.subtraction(a,b)
-                calculator.functions.displayLength();
+                calculator.functions.displayMaxLength();
             } else if (tempOperator == "*") {
                 display.textContent = this.multiplication(a,b)
-                calculator.functions.displayLength();
+                calculator.functions.displayMaxLength();
             } else if (tempOperator == "/") {
                 display.textContent = this.division(a,b)
-                calculator.functions.displayLength();
+                calculator.functions.displayMaxLength();
             } else {
                 return "ERROR";
             }
         },
     },
-    
+
+    // Contains all other neccessary functions
+
     functions: {
         plusMinus: function() {
             display.textContent = parseFloat(display.textContent) * -1;
         },
-        clear: function() {
+        allClear: function() {
             display.textContent = 0;
             firstOperand = 0;
             secondOperand = 0;
@@ -104,14 +107,14 @@ calculator = {
             tempOperator = operator;
         };
         },
-        btnInput: function(number) {
+        displayInput: function(number) {
             if (number == ".") {
                 display.textContent += "."
             } else {
                 (display.textContent === "0")? display.textContent = number : display.textContent += number;
         }
         },
-        displayLength: function() {
+        displayMaxLength: function() {
             if (display.textContent.length > 10) {
                 console.log(display.textContent)
                 return display.textContent = "ERROR"
@@ -119,10 +122,6 @@ calculator = {
         },
     }
 };
-
-
-
-
 
 //trying to get keyboard input to work. check javascript 30 drums on youtube.
 // window.addEventListener("keydown", function(e) {
