5dfd11b9b64bf1720e184084e13a4859a6fbb6ae
Gianluca Cioni <gianlucacioni93@gmail.com>
2021-07-19 03:31:46 +0200

    Cleaned the code

--- index.js
+++ index.js
@@ -1,30 +1,49 @@
-function btnInput(number) {
-    if (number == ".") {
-        display.textContent += "."
-    } else {
-        (display.textContent === "0")? display.textContent = number : display.textContent += number;
-}
-};
+let firstOperand = 0
 
-function btnOperator(operator) {
-    if (display.textContent != 0){
-    tempDisplay1 = parseFloat(display.textContent);
-    display.textContent = ""
-    tempOperator = operator;
-}
-}
+let secondOperand = 0
+
+let tempOperator = 0
+
+let display = document.querySelector("#display")
+
+allClearBtn.addEventListener("click", () => calculator.functions.clear())
+
+equalsBtn.addEventListener("click", () => calculator.operations.equals(tempOperator, firstOperand))
+
+divisionBtn.addEventListener("click", () => calculator.functions.operation("/"))
+
+multiplicationBtn.addEventListener("click", () => calculator.functions.operation("*"))
+
+additionBtn.addEventListener("click", () => calculator.functions.operation("+"))
+
+subtractionBtn.addEventListener("click", () => calculator.functions.operation("-"))
+
+plusMinusBtn.addEventListener("click", () => calculator.functions.plusMinus())
+
+zero.addEventListener("click", () => calculator.functions.btnInput(0))
+
+one.addEventListener("click", () => calculator.functions.btnInput(1))
+
+two.addEventListener("click", () => calculator.functions.btnInput(2))
+
+three.addEventListener("click", () => calculator.functions.btnInput(3))
+
+four.addEventListener("click", () => calculator.functions.btnInput(4))
+
+five.addEventListener("click", () => calculator.functions.btnInput(5))
+
+six.addEventListener("click", () => calculator.functions.btnInput(6))
+
+seven.addEventListener("click", () => calculator.functions.btnInput(7))
+
+eight.addEventListener("click", () => calculator.functions.btnInput(8))
+
+nine.addEventListener("click", () => calculator.functions.btnInput(9))
+
+point.addEventListener("click", () => calculator.functions.btnInput("."))
 
 
-function clear() {
-    display.textContent = 0;
-    tempDisplay1 = 0;
-    tempDisplay2 = 0;
-    operator = "";
-}
 
-function plusMinus() {
-    display.textContent = parseFloat(display.textContent) * -1;
-};
 
 calculator = {
     operations: {
@@ -44,116 +63,64 @@ calculator = {
             return a / b;
         },
 
-        equals: function(tempOperator,tempDisplay1) {
-            tempDisplay2 = parseFloat(display.textContent);
-            console.log(tempDisplay1)
-            console.log(tempDisplay2)
-            console.log(tempOperator)
-
-            return calculator.operations.operate(tempOperator,tempDisplay1,tempDisplay2)
-        
+        equals: function(tempOperator,firstOperand) {
+            secondOperand = parseFloat(display.textContent);
+            return calculator.operations.operate(tempOperator,firstOperand,secondOperand)
         },
         
         operate: function(tempOperator, a, b) {
             if (tempOperator == "+") {
-                console.log("hello")
                 display.textContent = this.addition(a,b);
-                return this.addition(a,b);
+                calculator.functions.displayLength();
             } else if (tempOperator == "-") {
                 display.textContent = this.subtraction(a,b)
-                return this.subtraction(a,b);
+                calculator.functions.displayLength();
             } else if (tempOperator == "*") {
                 display.textContent = this.multiplication(a,b)
-                return this.multiplication(a,b);
+                calculator.functions.displayLength();
             } else if (tempOperator == "/") {
                 display.textContent = this.division(a,b)
-                return this.division(a,b);
+                calculator.functions.displayLength();
             } else {
                 return "ERROR";
             }
         },
+    },
+    
+    functions: {
+        plusMinus: function() {
+            display.textContent = parseFloat(display.textContent) * -1;
+        },
+        clear: function() {
+            display.textContent = 0;
+            firstOperand = 0;
+            secondOperand = 0;
+            operator = "";
+        },
+        operation: function(operator) {
+            if (display.textContent != 0){
+            firstOperand = parseFloat(display.textContent);
+            display.textContent = ""
+            tempOperator = operator;
+        };
+        },
+        btnInput: function(number) {
+            if (number == ".") {
+                display.textContent += "."
+            } else {
+                (display.textContent === "0")? display.textContent = number : display.textContent += number;
+        }
+        },
+        displayLength: function() {
+            if (display.textContent.length > 10) {
+                console.log(display.textContent)
+                return display.textContent = "ERROR"
+            };
+        },
     }
-
 };
 
-var tempDisplay1 = 0
 
-var tempDisplay2 = 0
-
-var tempOperator = 0
-
-var display = document.querySelector("#display")
-
-let allClearBtn = document.querySelector("#allClear")
-
-let plusMinusBtn = document.querySelector("#plusMinus")
-
-let divisionBtn = document.querySelector("#division")
-
-let multiplicationBtn = document.querySelector("#multiplication")
-
-let subtractionBtn = document.querySelector("#subtraction")
-
-let additionBtn = document.querySelector("#addition")
-
-let equalsBtn = document.querySelector("#equals")
-
-let point = document.querySelector("#point")
-
-let nine = document.querySelector("#nine")
-
-let eight = document.querySelector("#eight")
-
-let seven = document.querySelector("#seven")
-
-let six = document.querySelector("#six")
-
-let five = document.querySelector("#five")
-
-let four = document.querySelector("#four")
-
-let three = document.querySelector("#three")
-
-let two = document.querySelector("#two")
-
-let one = document.querySelector("#one")
-
-
-allClearBtn.addEventListener("click", () => clear())
-
-equalsBtn.addEventListener("click", () => calculator.operations.equals(tempOperator, tempDisplay1))
-
-divisionBtn.addEventListener("click", () => btnOperator("/"))
-
-multiplicationBtn.addEventListener("click", () => btnOperator("*"))
-
-additionBtn.addEventListener("click", () => btnOperator("+"))
-
-subtractionBtn.addEventListener("click", () => btnOperator("-"))
-
-plusMinusBtn.addEventListener("click", () => plusMinus())
-
-zero.addEventListener("click", () => btnInput(0))
-
-one.addEventListener("click", () => btnInput(1))
-
-two.addEventListener("click", () => btnInput(2))
-
-three.addEventListener("click", () => btnInput(3))
-
-four.addEventListener("click", () => btnInput(4))
-
-five.addEventListener("click", () => btnInput(5))
-
-six.addEventListener("click", () => btnInput(6))
-
-seven.addEventListener("click", () => btnInput(7))
-
-eight.addEventListener("click", () => btnInput(8))
-
-nine.addEventListener("click", () => btnInput(9))
-
-point.addEventListener("click", () => btnInput("."))
 
 
 
